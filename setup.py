6f4a6f3cb58a3003260937eca545f2459b055b73
Ram Prasad Panda <ram082.iacr@gmail.com>
2018-02-20 11:33:04 +0530

    added pip install setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+setup(
+    author='Ram Prasad Panda',
+    author_email='pandar@timeinc.com',
+    install_requires=[
+                'boto3',
+                'botocore',
+        ],
+    license='GNU',
+    name='voltags',
+)
